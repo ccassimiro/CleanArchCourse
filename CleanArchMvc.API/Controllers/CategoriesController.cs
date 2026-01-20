51239e0deec116b650dbdc2cb81a6751882af067
lcassimiro1 <lcassimiro1@gmail.com>
2026-01-20 16:46:25 -0300

    wip: adding category api

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,89 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+
+            if (categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> GetById(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = await _categoryService.GetByIdAsync(id.Value);
+
+                if (category == null)
+                {
+                    return NotFound("Category not found");
+                }
+
+                return Ok(category);
+            }
+
+            return NotFound("Category not found");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CategoryDTO categoryDto)
+        {
+            if (categoryDto == null)
+                return BadRequest("Invalid data.");
+
+            await _categoryService.AddAsync(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.Id }, categoryDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(int id, [FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id || categoryDto == null)
+                return BadRequest("Invalid data.");
+
+            await _categoryService.UpdateAsync(categoryDto);
+
+            return Ok(categoryDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if(id == null)
+                return BadRequest("Invalid data.");
+
+            var category = await _categoryService.GetByIdAsync(id);
+            if (category == null)
+                return NotFound("Category not found");
+
+            await _categoryService.RemoveAsync(id.Value);
+
+            return Ok(category);
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Infrastructure dependencies registration goes here
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //repositories
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            //services
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myHandlers);
+
+            return services;
+        }
+    }
+}
